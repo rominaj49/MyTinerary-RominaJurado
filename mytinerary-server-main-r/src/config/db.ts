a8852f45c6fbc79ce2dd4f51d1711da73c3f85e1
rominaj49 <rominajurado49@gmail.com>
2024-03-15 12:53:32 -0300

    cambios

--- mytinerary-server-main-r/src/config/db.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose'
-import config from '../types/config'
-export default mongoose.connect(config.api.MONGO_URI || "")
-  .then(() => console.log('data base connected'))
-  .catch(err => console.log(err)) 
--- mytinerary-server-main-r/src/utils/catchedAsync.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from "express" 
-export const catchedAsync = (fn: Function) => (req: Request, res: Response, next: NextFunction) => fn(req, res).catch( (err:Error) => next(err) )
-        
-    
